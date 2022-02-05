f3021e81ee7904699f9c65c1e9b4a3f4381bd4b9
DESKTOP-ETJC3U7\honglab403 <icechocola05@gmail.com>
2022-02-05 15:18:50 +0900

    #2 로그인, 회원가입 기능 구현
    - 공통 CSS는 base.css
    - User DAO, DTO 추가

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -4,17 +4,10 @@
 <html>
 <head>
 <title>Story Reader</title>
+<link rel="stylesheet" href="CSS/index.css" type='text/css' >
 </head>
-<%@ include file="head.html" %>
+<%@ include file="header.jsp" %>
 <body>
-	<div class="header">
-		<div class="w3-bar w3-border w3-card-4 w3-xlarge">
-			<a class="w3-bar-item w3-button w3-hover-none w3-text-black w3-hover-text-white w3-padding-24 w3-margin-left" href="#">동화 만들기</a>
-			<a class="w3-bar-item w3-button w3-hover-none w3-text-black w3-hover-text-white w3-padding-24" href="#">만든 동화 보기</a>
-			<a class="w3-bar-item w3-button w3-hover-none w3-text-black w3-hover-text-white w3-padding-24" href="#">도움말</a>
-			<a class="w3-bar-item w3-button w3-hover-none w3-text-black w3-hover-text-white w3-padding-24 w3-right w3-margin-right" href="login.jsp">로그인</a>
-		</div>
-	</div>
 	<div class="contents">
 		<div class="sub-title">당신의 이야기를 읽어드릴게요</div>
 		<div class="title">Story Reader</div>
---
+++ src/model/dao/UserDAO.java
@@ -0,0 +1,125 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.dto.User;
+
+public class UserDAO {
+		// 로그인
+	   private final static String SQLST_SELECT_USER_BY_ID = "SELECT user_id, user_login_id, user_login_pw, user_name FROM user WHERE user_login_id = ?";
+	   //회원가입
+	   private final static String SQLST_INSERT_USER = "INSERT INTO user(user_login_id, user_login_pw, user_name) VALUES(?, ?, ?)";
+	   // 아이디 중복 확인
+	   private final static String SQLST_SELECT_USER_ID = "SELECT user_login_id FROM user WHERE user_login_id = ?";
+	   
+	   /* 로그인 - user의 id로 pw 정보를  가져오기
+	    * 
+	    * 가져온 pw가 파라미터 pw와 같을 때 return user id
+	    * 가져온 pw가 파라미터 pw와 다를 때 return null
+	    * 가져올 수 있는 pw가 없을 때(user가 없을 때) return null
+	    */
+	   public static User findUser(Connection con, String user_input_id, String user_input_pw) throws SQLException {
+			PreparedStatement pstmt = null;
+			User user = new User();
+			try {
+				con.setAutoCommit(false);
+				pstmt = con.prepareStatement(SQLST_SELECT_USER_BY_ID);
+				pstmt.setString(1, user_input_id);
+				ResultSet rs = pstmt.executeQuery();
+				con.commit();
+				con.setAutoCommit(true);
+				
+				if(rs.next()) { //existing user
+					String user_login_pw = rs.getString(3);
+					if(user_login_pw.equals(user_input_pw)) { //login success
+						//fill user model
+						user.setUserId(rs.getInt(1));
+						user.setUserLoginId(rs.getString(2));
+						user.setUserLoginPw(rs.getString(3));
+						user.setUserName(rs.getString(4));
+						return user;
+					}
+					else { //login failed
+						return null;
+					}
+				}
+				else { //invalid user
+					return null;
+				}
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				 if(pstmt != null) {
+			        pstmt.close(); 
+			     }
+			}
+			return null;
+		}
+	   
+
+	   
+	   /* 주어진 id가 DB내 존재하는지 확인(아이디 중복확인)
+	    * 
+	    * 주어진 id가 DB내 존재하지 않을 경우 return true
+	    * 주어진 id가 DB내 존재하거나 쿼리 실행 실패 시 return false
+	    */
+	   public static boolean selectID(Connection con, String id) throws SQLException{
+	      PreparedStatement pstmt=null;
+	      try {
+	         con.setAutoCommit(false);
+	         pstmt = con.prepareStatement(SQLST_SELECT_USER_ID);
+	         pstmt.setString(1, id);
+	         ResultSet rs = pstmt.executeQuery();
+	         con.commit();
+	         con.setAutoCommit(true);
+	         if (rs.next()) {
+	            return false;
+	         } else {
+	            return true; // 사용가능한 아이디(email)
+	         }
+	         
+	      } catch (SQLException e) {
+	         e.printStackTrace();
+	         return false;
+	      } finally {
+	         if(pstmt != null) {
+	            pstmt.close(); 
+	         }
+	      }
+	   }
+	   
+	   /* 주어진 사용자 정보를 DB에 삽입(회원가입)
+	    * 
+	    * 성공 시 return true
+	    * 실패 시 return false
+	    */
+	   public static boolean insertUser(Connection con, User user) throws SQLException{
+	      PreparedStatement pstmt=null;
+	      try {
+	         con.setAutoCommit(false);
+	         pstmt = con.prepareStatement(SQLST_INSERT_USER);
+	         pstmt.setString(1, user.getUserLoginId());
+	         pstmt.setString(2, user.getUserLoginPw());
+	         pstmt.setString(3, user.getUserName());
+	         int insertCount = pstmt.executeUpdate();
+	         con.commit();
+	         con.setAutoCommit(true);
+	         if (insertCount == 1) { // 회원정보 삽입에 성공한다면
+	            return true;
+	         } else {
+	            return false;
+	         } 
+	      }catch (Exception e) {
+	         e.printStackTrace();
+	         return false;
+	      }finally {
+	         if(pstmt != null) {
+	            pstmt.close(); 
+	         }
+	      }
+	   }
+}
